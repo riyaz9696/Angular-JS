64d61795aaee56bebe9e6f386ed72deb7fe71c11
prabhat <prabhat2346@gmail.com>
2019-01-11 00:43:26 +0530

    some errors

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -8,16 +8,24 @@ import { of as observableof } from 'rxjs/observable/of'
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+}
  
 @Injectable()
 export class HeroService {
  
-  constructor(private messageService: MessageService) { }
+  constructor(
+  private http: HttpClient,
+  private messageService: MessageService,
+  private log(message: string){
+  this.messageService.add('HeroService: ${message}');
+  }
+  ) { }
  
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
@@ -25,6 +33,9 @@ export class HeroService {
     return of(HEROES.find(hero => hero.id === id));
   }
 }
+
+
+
  
 
    
